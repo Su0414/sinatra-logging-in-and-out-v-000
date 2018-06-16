c6dd0fe6d38733136431b665116ff66e286a7a21
Suchitra Naik <susanaik@gmail.com>
2018-06-16 21:35:20 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
-  def current_user(session)
+  def self.current_user(session)
     User.find_by(id: session[:user_id])
   end 
 
-  def is_logged_in?(session)
-    if current_user(session)
+  def self.is_logged_in?(session)
+    if self.current_user(session)
       return true 
     else 
       return false 
