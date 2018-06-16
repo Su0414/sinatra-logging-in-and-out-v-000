bed8d3dd22c4692566344dd92037e85d79fa1834
Suchitra Naik <susanaik@gmail.com>
2018-06-16 21:08:56 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,15 @@
 class Helpers
+
+  def current_user(sessions)
+    User.find_by(username: sessions[:username])
+  end
+
+  def is_logged_in?(sessions)
+    if current_user(sessions)
+      return true
+    else
+      return false
+    end
+  end
+
 end
